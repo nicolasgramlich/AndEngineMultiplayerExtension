b643c9b6ac7667918dae2ae538b2297ac8a7bf94
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-06-22 22:44:46 +0200

    Added Empty Client/Server Messages that have only flags no data.

---
+++ src/org/anddev/andengine/extension/multiplayer/protocol/adt/message/client/EmptyClientMessage.java
@@ -0,0 +1,57 @@
+package org.anddev.andengine.extension.multiplayer.protocol.adt.message.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 22:27:39 - 22.06.2010
+ */
+public abstract class EmptyClientMessage extends BaseClientMessage {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public EmptyClientMessage() {
+		/* Nothing to store. */
+	}
+
+	public EmptyClientMessage(final DataInputStream pInputStream) throws IOException {
+		/* Nothing to read. */
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	protected void onWriteTransmissionData(final DataOutputStream pDataOutputStream) throws IOException {
+		/* Nothing to write. */
+	}
+
+	@Override
+	protected void onAppendTransmissionDataForToString(final StringBuilder pStringBuilder) {
+		/* Nothing to append. */
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
