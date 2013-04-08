bd7e347b3eb6953624e7433a746c44217b3c05c6
Nicolas Gramlich <ngramlich@zynga.com>
2013-04-07 20:37:10 -0700

    Fixed imports so AndEngineMultiplayerExtension compiles with plain javac.

--- src/org/andengine/extension/multiplayer/protocol/server/BluetoothSocketServer.java
+++ src/org/andengine/extension/multiplayer/protocol/server/BluetoothSocketServer.java
@@ -3,11 +3,11 @@ package org.andengine.extension.multiplayer.protocol.server;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.andengine.extension.multiplayer.protocol.server.connector.ClientConnector;
+import org.andengine.extension.multiplayer.protocol.shared.BluetoothSocketConnection;
 import org.andengine.extension.multiplayer.protocol.server.BluetoothSocketServer.IBluetoothSocketServerListener.DefaultBluetoothSocketServerListener;
 import org.andengine.extension.multiplayer.protocol.server.connector.BluetoothSocketConnectionClientConnector.DefaultBluetoothSocketClientConnectorListener;
-import org.andengine.extension.multiplayer.protocol.server.connector.ClientConnector;
 import org.andengine.extension.multiplayer.protocol.server.connector.ClientConnector.IClientConnectorListener;
-import org.andengine.extension.multiplayer.protocol.shared.BluetoothSocketConnection;
 import org.andengine.util.Bluetooth;
 import org.andengine.util.debug.Debug;
 import org.andengine.util.exception.BluetoothException;
